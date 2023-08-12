935263a6d8f9174b03a5901b6166f2aab93a8268
tafazzolian <tafazzolyan@gmail.com>
2023-08-13 01:14:13 +0330

    borrowing phase

--- library/urls.py
+++ library/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 app_name = 'library'
 urlpatterns = [
-    #path('library/',views.library.as_view(), name='library_view'),
+    path('books/<int:books_id>/',views.borrow_book, name='borrow'),
     path('',views.HomePage.as_view(), name='home'),
 
 ]
--- library/views.py
+++ library/views.py
@@ -1,10 +1,49 @@
-from django.shortcuts import render
 from django.views import View
-from library.models import Books
-#from .forms import SearchForm
+from library.models import Books,BorrowedBook
 from django.contrib.auth.models import User
+from datetime import date
+from django.shortcuts import get_object_or_404
+from django.contrib.auth.decorators import login_required
+from django.contrib import messages
+from django.shortcuts import render, redirect
+
+
+@login_required
+def borrow_book(request, books_id):
+    book = get_object_or_404(Books, id=books_id)
+    user = request.user
+    borrowed_books_count = BorrowedBook.objects.filter(user=user).count()
+
+    if borrowed_books_count >= 5:
+        messages.error(request, "You have reached the maximum number of borrowed books.")
+    elif book.copies_available <= 0:
+        messages.error(request, "No copies of this book are currently available.")
+    else:
+        # Create a new BorrowedBook instance
+        borrowed_book = BorrowedBook(user=user, book=book, borrow_date=date.today())
+        borrowed_book.save()
+        book.copies_available -= 1
+        book.save()
+        messages.success(request, f"You have successfully borrowed '{book.title}'.")
+    #return render(request,'main.html',{'books':book})
+    return redirect('library:home')
+
+@login_required
+def return_book(request, borrowed_book_id):
+    borrowed_book = get_object_or_404(BorrowedBook, id=borrowed_book_id)
+
+    # Set the return date and increase the available copies
+    borrowed_book.return_date = date.today()
+    borrowed_book.book.copies_available += 1
+
+    borrowed_book.book.save()
+    borrowed_book.save()
+    messages.success(request, "You have successfully returned the book.")
+
+    return redirect('library:home')
 
 class HomePage(View):
     def get(self,request):
         books = Books.objects.all()
-        return render(request,'main.html',{'books':books})
+        borrowed_books = BorrowedBook.objects.all()
+        return render(request,'main.html',{'books':books,'borrowed_books': borrowed_books,})
