e1a861e0c8939abb2feb0059b39ae83f0f495b01
tafazzolian <tafazzolyan@gmail.com>
2023-08-14 01:02:59 +0330

    workin on it

--- library/urls.py
+++ library/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 app_name = 'library'
 urlpatterns = [
-    path('books/<int:books_id>/',views.borrow_book, name='borrow'),
+    path('books/<int:books_id>/',views.BorrowBook.as_view(), name='borrow'),
     path('',views.HomePage.as_view(), name='home'),
 
 ]
--- library/views.py
+++ library/views.py
@@ -6,27 +6,41 @@ from django.shortcuts import get_object_or_404
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
 from django.shortcuts import render, redirect
+from .forms import DateInputForm
 
 
-@login_required
-def borrow_book(request, books_id):
-    book = get_object_or_404(Books, id=books_id)
-    user = request.user
-    borrowed_books_count = BorrowedBook.objects.filter(user=user).count()
 
-    if borrowed_books_count >= 5:
-        messages.error(request, "You have reached the maximum number of borrowed books.")
-    elif book.copies_available <= 0:
-        messages.error(request, "No copies of this book are currently available.")
-    else:
-        # Create a new BorrowedBook instance
-        borrowed_book = BorrowedBook(user=user, book=book, borrow_date=date.today())
-        borrowed_book.save()
-        book.copies_available -= 1
-        book.save()
-        messages.success(request, f"You have successfully borrowed '{book.title}'.")
-    #return render(request,'main.html',{'books':book})
-    return redirect('library:home')
+class BorrowBook(View):
+    form_class = DateInputForm
+    
+    def get(self,request, books_id):
+        get_object_or_404(Books, id=books_id)
+        form = self.form_class()
+        return render(request,'main.html',{'form':form})
+    
+    @login_required
+    def post(self,request, books_id):
+        form = self.form_class(request.POST)    
+        book = get_object_or_404(Books, id=books_id)
+        user = User.objects.get(id=user_id)
+        borrowed_books_count = BorrowedBook.objects.filter(user=user).count()
+        if borrowed_books_count >= 5:
+            messages.error(request, "You have reached the maximum number of borrowed books.")
+        elif book.copies_available <= 0:
+            messages.error(request, "No copies of this book are currently available.")
+        elif form.is_valid():
+            selected_date = form.cleaned_data['date']
+            borrowed_book = BorrowedBook(user=user, book=book, borrow_date=date.today(), return_date=selected_date)
+            borrowed_book.save()
+            book.copies_available -= 1
+            book.save()
+            messages.success(request, f"You have successfully borrowed '{book.title}'.")
+                # Do something with the selected_date
+        else:
+            self.form_class() 
+            
+        return render(request,'main.html',{'form':form})
+        #return redirect('library:home')
 
 @login_required
 def return_book(request, borrowed_book_id):
