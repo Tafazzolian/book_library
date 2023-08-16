272ad4d78ed0e9aeea6f964fa8b9d2084856645f
tafazzolian <tafazzolyan@gmail.com>
2023-08-16 10:26:48 +0330

    bugfix1

--- library/views.py
+++ library/views.py
@@ -3,26 +3,26 @@ from library.models import Books,BorrowedBook
 from django.contrib.auth.models import User
 from datetime import date
 from django.shortcuts import get_object_or_404
-from django.contrib.auth.decorators import login_required
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib import messages
 from django.shortcuts import render, redirect
 from .forms import DateInputForm
 
 
 
-class BorrowBook(View):
+class BorrowBook(LoginRequiredMixin,View):
     form_class = DateInputForm
+    template_class = 'library/borrow.html'
     
     def get(self,request, books_id):
-        get_object_or_404(Books, id=books_id)
+        books = get_object_or_404(Books, id=books_id)
         form = self.form_class()
-        return render(request,'main.html',{'form':form})
+        return render(request,self.template_class,{'form':form,'books':books})
     
-    @login_required
     def post(self,request, books_id):
         form = self.form_class(request.POST)    
         book = get_object_or_404(Books, id=books_id)
-        user = User.objects.get(id=user_id)
+        user = User.objects.get(id=request.user.id)
         borrowed_books_count = BorrowedBook.objects.filter(user=user).count()
         if borrowed_books_count >= 5:
             messages.error(request, "You have reached the maximum number of borrowed books.")
@@ -35,14 +35,11 @@ class BorrowBook(View):
             book.copies_available -= 1
             book.save()
             messages.success(request, f"You have successfully borrowed '{book.title}'.")
-                # Do something with the selected_date
         else:
             self.form_class() 
             
-        return render(request,'main.html',{'form':form})
-        #return redirect('library:home')
-
-@login_required
+        return redirect('library:home')
+    
 def return_book(request, borrowed_book_id):
     borrowed_book = get_object_or_404(BorrowedBook, id=borrowed_book_id)
 
